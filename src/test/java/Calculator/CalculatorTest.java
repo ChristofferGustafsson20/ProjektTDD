52b42b2b41ff2852b78741de1256587b5433cc8d
ChristofferGustafsson20 <Christoffererikgustafsson@gmail.com>
2020-12-03 18:26:08 +0100

    2

--- src/test/java/Calculator/CalculatorTest.java
+++ src/test/java/Calculator/CalculatorTest.java
@@ -1,15 +1,28 @@
 package Calculator;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
+    private static Calculator calculator;
 
+    @BeforeAll
+    public static void beforeAll(){
+        calculator = new Calculator();
+    }
 
     @Test
     void shouldReturnZero(){
-        Calculator calculator = new Calculator();
-        assertEquals(calculator.stringcalculator(""),0);
+        assertEquals(calculator.add(""),0);
+    }
+    @Test
+    void shouldReturnOne(){
+        assertEquals(calculator.add("1"),1);
+    }
+    @Test
+    void shouldReturnNumbersSeperated(){
+        assertEquals(calculator.add("1,2"),3);
     }
 }
