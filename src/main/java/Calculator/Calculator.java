6a6dbbca498f02fc08d389089a0cea7a3f334239
ChristofferGustafsson20 <Christoffererikgustafsson@gmail.com>
2020-12-03 16:03:00 +0100

    1

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -1,4 +1,9 @@
 package Calculator;
 
 public class Calculator {
+
+
+    public int stringcalculator(String numbers) {
+        return Integer.parseInt(numbers);
+    }
 }
--- src/test/java/Calculator/CalculatorTest.java
+++ src/test/java/Calculator/CalculatorTest.java
@@ -1,4 +1,15 @@
 package Calculator;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalculatorTest {
+
+
+
+    @Test
+    void shouldReturnZero(){
+        Calculator calculator = new Calculator();
+        assertEquals(calculator.stringcalculator(""),0);
+    }
 }
